ab95a2d634e046719f8fbf326c924cc2ef92dca2
corretorpaulocotrim <corretorpaulocotrim@gmail.com>
2026-07-19 15:12:51 -0300

    Bump SW cache v27->v28 (cinema sitewide)

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Network-First para HTML
    ===================================================== */
 
-const CACHE_NAME = 'paulocotrim-v27';
+const CACHE_NAME = 'paulocotrim-v28';
 const CACHE_STATIC = 'paulocotrim-static-v15';
 
 // Arquivos essenciais para cache (carregam offline)
